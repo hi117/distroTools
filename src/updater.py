ef21800e91bd05f3ec7f9ac79414d1e6d955eeab
hi117 <hi117.contact@gmail.com>
2012-12-17 16:37:12 -0500

    almost finished debuggind updater, also added config

--- src/updater.py
+++ src/updater.py
@@ -1,12 +1,17 @@
 import os
 import os.path
 from multiprocessing import Process,Pipe
-from importhelper import load
+from utils.importhelper import load
 from kyotocabinet import *
-import debug as storage
+from utils.configParser import parse as confParse
 
 db = DB()
 assert db.open('pkgverdb.kch', DB.OWRITER | DB.OREADER | DB.OCREATE), 'error opening db'
+
+def processConfig(config):
+    config['hoppers'] = map(lambda a: a.strip(), config['hoppers'].split(','))
+    config['storages'] = map(lambda a: a.strip(), config['storages'].split(','))
+    return config
 
 def buildpkg(data):
     '''
@@ -15,62 +20,83 @@ def buildpkg(data):
     '''
     # determine the PKGBUILD.in to use
     if not data[0] in os.listdir('pkgs'): 
-        print(data[0]+' not in directory')
+        print(data[0] + ' not in directory')
         return
-    path='pkgs/'+data[0]
-    PKGBUILD=open(path+'/PKGBUILD.in','r').read().replace('%n',data[0]).replace('%v',data[1]).replace('%%','%')
+    path = 'pkgs/'+data[0]
+    PKGBUILD = open(path + '/PKGBUILD.in', 'r').read().replace('%n', data[0]).replace('%v', data[1]).replace('%r', data[2]).replace('%%', '%')
     
-    # send it to the storage module
-    storage.store(path,PKGBUILD)
+    # send it to the storage modules
+    for i in storages:
+        i.store(path, PKGBUILD)
 
 def process(data):
     '''
-    data is a touple of (package,version)
+    data is a touple of (package, version, rev)
     this funciton determines if an update is needed and builds it
     '''
+    print('Recieved data: ' + ','.join(data))
     # determine if the version they gave is newer than the current version
-    ver=''
+    ver = ''
     for i in data[1]:
         if i.isdigit():
-            ver+=i
-    vercur=''
-    if db[data[0]]:
-        for i in db[data[0]]:
-            if i.isdigit():
-                vercur+=i
-    if vercur == '': vercur='-1'
-    if int(ver) > int(vercur):
-        # the version is newer
-        buildpkg(data)
-        db[data[0]]=data[1]
+            ver += i
 
+    rev = int(data[2])
+    revcur = '1'
+
+    vercur = ''
+    if db[data[0]]:
+        for i in db[data[0]].split(',')[0]:
+            if i.isdigit():
+                vercur += i
+        # grab the revision number
+        revcur = int(db[data[0]].split(',')[1])
+
+    if vercur == '': vercur = '-1'
+
+    if int(ver) > int(vercur):
+        if rev > int(revcur):
+            # the version is newer
+            buildpkg(data)
+            db[data[0]] = ','.join(data[1], data[2])
+
+# parse the configuration file for options
+config = confParse('updater.conf')['']
+
+# do additional processing of the config
+config = processConfig(config)
 
 # create a pipe for other processes to use
 recvp, sendp = Pipe()
 
 # load the hoppers
-modules=[]
-for i in os.listdir('hoppers'):
-    if os.path.isdir('hoppers/'+i):
-        continue
-    modules.append(load('hoppers/'+i))
+modules = []
+for i in config['hoppers']:
+    modules.append(load(config['hopperdir'] + '/' + i + '.py'))
+
+# load the storage modules
+storages = []
+for i in config['storages']:
+    print('Loading storage module: ' + i)
+    storages.append(load(config['storagedir'] + '/' + i + '.py'))
 
 # run the hoppers
-hopperp=[]
+hopperp = []
 for i in modules:
-    print("Starting "+i.__str__())
-    p = Process(target=i.run, args=(sendp,))
+    print("Starting " + i.__str__())
+    p = Process(target = i.run, args = (sendp,))
     p.start()
     hopperp.append(p)
 
 # listen on pipe, when a connection is recieved, process it and check the process list
-# NOBODY SHOULD CLOSE THE DAMN PIPE
-while len(hopperp)!=0:
-    data=recvp.recv()
+# NOBODY SHOULD CLOSE THE PIPE
+while len(hopperp) != 0:
+    data = recvp.recv()
     process(data)
-    while n<len(hopperp):
+    n = 0
+    while n < len(hopperp):
         if hopperp[n].is_alive():
             hopperp[n].join()
             hopperp.pop(n)
         else:
-            n+=1
+            n += 1
