ddb1ad23dc4e7f27f79d4977c51480e1188bc659
hi117 <hi117.contact@gmail.com>
2012-12-29 18:26:53 -0500

    added some to builder and storage/sshfs

--- src/builder.py
+++ src/builder.py
@@ -3,6 +3,13 @@ from utils.importhelper import load
 
 config = {}
 
+class package:
+    def __init__(self, name, config):
+        self.name = name
+        self.config = config
+        self.hasBuilt = False
+        self.hasError = None
+
 def processConfig(config):
     config['storages'] = map(lambda a: a.strip(), config['storages'].split(','))
     return config
--- src/storage/sshfs.py
+++ src/storage/sshfs.py
@@ -7,8 +7,15 @@ from shutil import rmtree, copytree as rm, cp
 config = configParser.parse('sshfs.conf')['']
 
 # mount the point (if necessary)
-if call(['mountpoint', '-q', config['mountpoint']): 
+if call(['mountpoint', '-q', config['mountpoint']]): 
         call(['sshfs', '-oPort=' + config['port'], config['host'] + ':/', config['mountpoint']])
+
+def procPkgInfo(PKGBUILD):
+    '''
+    grabs the pkvger and pkgname from the package
+    '''
+    for line in PKGBUILD.split("\n"):
+        if strip(line) [:7] 
 
 # now implement functions
 def store(path, PKGBUILD):
@@ -30,3 +37,17 @@ def store(path, PKGBUILD):
     # put the PKGBUILD in the new directory
     with open(config['mountpoint'] + '/' +  config['basedir'] + '/' + path.split('/')[-1] + '/' + 'PKGBUILD', 'w') as f:
         f.write(PKGBUILD)
+
+    # process the PKGBUILD for name and version info
+    procPkgInfo(PKGBUILD):
+
+def get(package, path):
+    '''
+    for this function, package is a package name and path is a directory to put it in
+    '''
+    # ensure the mountpoint is still mounted
+    if not call(['mountpoint', '-q', config['mountpoint']]):
+        call(['sshfs', '-oPort=' + config['port'], config['host'], config['mountpoint']])
+
+    # copy the package directory to the new path
+    cp(cpnfig['mountpoint'] + '/' + config['basedir'] + '/' + package, path)
