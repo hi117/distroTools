1ecd1a903824629a1a7dc371a1d6b018e5f263ab
hi117 <hi117.contact@gmail.com>
2012-12-29 16:46:50 -0500

    committed for no reason really

--- src/builder.py
+++ src/builder.py
@@ -1,1 +1,10 @@
 from multiprocessing import Pipe, Process
+from utils.importhelper import load
+
+config = {}
+
+def processConfig(config):
+    config['storages'] = map(lambda a: a.strip(), config['storages'].split(','))
+    return config
+
+
