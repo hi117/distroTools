1057fd1df423e0fc139c218068e63b3844a46180
hi117 <hi117.contact@gmail.com>
2012-12-30 14:38:29 -0500

    Got the theory for building packages. We build a graph with edges for depends and required by's going down a depends edges is considered going in the down direction going down a required by edges is considered going in the up direction if we go all the way down, we will find a base package, and we can build that safely after each one is buit, we re-trace the graph going as far down as possible until we hit bottom and build that one a circular dependancy can be detected by keeping a list of visited packages.  if we find a package already on our list while going down, we assume we hit bottom and we build all packages between the matched indice and the one at the bottom once, and then build the first package again.  an example of this is the linux buildchain described as so: binutilsi->gcc->glibc->binutils->gcc in this case, we have nested circular dependancies. the first case occurs in the binutils->...->bintuils case and the second in the gcc->...->gcc case.  in this case, glibc would be built twice and gcc would be built three times by this algorithm.
    This should ensure a proper package output.  Also some packages may need
    a first pass version like libav, which requires --enable-libx264 to be
    turned off on the first pass.  I will likely use a mangle chain in the
    actual builder to handle this.  Also in the mangle chain may be a way of
    modifying the build list from the mangle chain.  this way, if there is
    an incompatible API change in the required by packages, we can avoid
    building this package. though this is a project for the future.

--- src/builder.py
+++ src/builder.py
@@ -1,17 +1,70 @@
 from multiprocessing import Pipe, Process
 from utils.importhelper import load
+from os import listdir
 
 config = {}
+gpkginfo = None # dict is in form {pkg: (deps, reqby)}
+
+'''
+These classes below basically simulate structs as in C.
+'''
 
 class package:
-    def __init__(self, name, config):
+    def __init__(self, name):
         self.name = name
-        self.config = config
         self.hasBuilt = False
         self.hasError = None
+        self.config = None
+        self.deps = []
+        self.reqby = []
+
+class buildOrder:
+    def __init__(self, config):
+        self.pkgs = []
+        self.config = config
 
 def processConfig(config):
-    config['storages'] = map(lambda a: a.strip(), config['storages'].split(','))
+    '''
+    Does additional processing to the config.
+    '''
+    config['storage'] = map(lambda a: a.strip(), config['storage'].split(','))
     return config
 
+def buildInitGPkgInfo():
+    '''
+    This functon builds the inital gpkginfo dict.
 
+    '''
+    for i in listdir(config['basedir']):
+        # load the PKGBUILD
+        pkgbuild = open(config['basedir'] + '/' + i + '/PKGBUILD').readlines()
+
+        # enumerate over lines stripping and looking for depends, makedepends
+        for i in pkgbuild:
+            i = i.strip(i)
+            if i.split('=')[0] == 'depends' or i.split('=')[0] == 'makedepsnds':
+                
+
+def getDeps(pkg):
+    '''
+    This function returns a deps, reqby topule
+    '''
+    if not gpkginfo:
+        buildInitGPkgInfo()
+
+def buildPkgs(pkgs):
+    '''
+    This function takes a list of package names and builds them in the proper order.
+    '''
+    # build and populate the order struct
+    order = buildOrder()
+    for i in pkgs:
+        order.pkgs.append(package(i))
+    
+    # copy over all the packages from storage to the local dir
+    for i in order.pkgs:
+        storage.get(i, config['basedir'])
+
+    # build the graph
+    for i in order.pkgs:
+        deps = getDeps(i)
