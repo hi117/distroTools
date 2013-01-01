edfc4b32359ae2621f3b828c190bec8ce6901e61
hi117 <hi117.contact@gmail.com>
2013-01-01 14:36:59 -0500

    simplified the coding and finished the order graph building

--- src/builder.py
+++ src/builder.py
@@ -3,7 +3,6 @@ from utils.importhelper import load
 from os import listdir
 
 config = {}
-gpkginfo = None # dict is in form {pkg: (deps, reqby)}
 
 '''
 These classes below basically simulate structs as in C.
@@ -18,7 +17,7 @@ class package:
         self.deps = []
         self.reqby = []
 
-class buildOrder:
+class Order:
     def __init__(self, config):
         self.pkgs = []
         self.config = config
@@ -30,41 +29,54 @@ def processConfig(config):
     config['storage'] = map(lambda a: a.strip(), config['storage'].split(','))
     return config
 
-def buildInitGPkgInfo():
+def getPkgFromOrder(order, pkg):
+    '''
+    This function takes a pkg by name and gets the package class from the order
+    '''
+    for i in order.pkgs:
+        if i.name == pkg:
+            return i
+    return None
+
+def buildOrder(order):
     '''
     This functon builds the inital gpkginfo dict.
 
     '''
-    for i in listdir(config['basedir']):
+    for p in listdir(config['basedir']):
         # load the PKGBUILD
-        pkgbuild = open(config['basedir'] + '/' + i + '/PKGBUILD').readlines()
+        pkgbuild = open(config['basedir'] + '/' + p + '/PKGBUILD').readlines()
 
         # enumerate over lines stripping and looking for depends, makedepends
+        dependancies = []
         for i in pkgbuild:
             i = i.strip(i)
             if i.split('=')[0] == 'depends' or i.split('=')[0] == 'makedepsnds':
-                
+                deps = map(lambda a: a.split("()'\""), i.split('=')[1].split(' '))
+                for j in deps:
+                    if not j in dependancies:
+                        dependancies.apend(j)
 
-def getDeps(pkg):
-    '''
-    This function returns a deps, reqby topule
-    '''
-    if not gpkginfo:
-        buildInitGPkgInfo()
+        # get the pkg from the order and add the dependancies to it
+        pkg = getPkgFromOrder(order, p)
+        for i in dependancies:
+            dep = getPkgFromOrder(order, i)
+            if dep:
+                pkg.deps.append(dep)
+
+        # for each dependancy, add pkg to its reqby list
+        for i in pkg.deps:
+            i.reqby.append(pkg)
+    return order
 
 def buildPkgs(pkgs):
     '''
     This function takes a list of package names and builds them in the proper order.
     '''
     # build and populate the order struct
-    order = buildOrder()
-    for i in pkgs:
-        order.pkgs.append(package(i))
+    order = Order()
+    order = buildOrder(order)
     
     # copy over all the packages from storage to the local dir
-    for i in order.pkgs:
+    for i in pkgs:
         storage.get(i, config['basedir'])
-
-    # build the graph
-    for i in order.pkgs:
-        deps = getDeps(i)
